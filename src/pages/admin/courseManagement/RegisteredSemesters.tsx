4ad250d2d79a76270bae86d9fd3299baca2454b7
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-12-28 01:27:25 +0600

    Showing month using moment

--- src/pages/admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -1,6 +1,7 @@
 import { Button, Table, TableColumnsType, TableProps } from 'antd';
 import { TAcademicSemester } from '../../../types/academicManagement.type';
 import { useGetAllRegisteredSemestersQuery } from '../../../redux/features/admin/courseManagement.api';
+import moment from 'moment';
 
 export type TTableData = Pick<
   TAcademicSemester,
@@ -20,8 +21,8 @@ const RegisteredSemesters = () => {
     ({ _id, academicSemester, startDate, endDate, status }) => ({
       key: _id,
       name: `${academicSemester.name} ${academicSemester.year}`,
-      startDate,
-      endDate,
+      startDate: moment(new Date(startDate)).format('MMMM'),
+      endDate: moment(new Date(endDate)).format('MMMM'),
       status,
     })
   );
