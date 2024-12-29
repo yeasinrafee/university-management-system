911defeeb8d5ba176acad6af7beca646a13fd425
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-12-29 12:08:47 +0600

    Student enroll course ui

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,8 +1,60 @@
+import { Button, Col, Row } from 'antd';
+import { useGetAllOfferedCoursesQuery } from '../../redux/features/student/studentCourseManagement.api';
+
 const OfferedCourse = () => {
+  const { data: offeredCourseData } = useGetAllOfferedCoursesQuery(undefined);
+
+  const singleObject = offeredCourseData?.data?.reduce((acc, item) => {
+    const key = item.course.title;
+
+    acc[key] = acc[key] || { courseTitle: key, section: [] };
+    acc[key].section.push({
+      section: item.section,
+      _id: item._id,
+      days: item.days,
+      startTime: item.startTime,
+      endTime: item.endTime,
+    });
+
+    return acc;
+  }, {});
+
+  const modifiedData = Object.values(singleObject ? singleObject : {});
+
   return (
-    <div>
-      <h1>Offered Course for Student</h1>
-    </div>
+    <Row gutter={[0, 20]}>
+      {modifiedData.map((item) => {
+        return (
+          <Col span={24} style={{ border: 'solid #d4d4d4 2px' }}>
+            <div style={{ padding: '10px' }}>
+              <h2>{item.courseTitle}</h2>
+            </div>
+            <div>
+              {item.section.map((section) => {
+                return (
+                  <Row
+                    justify='space-between'
+                    align='middle'
+                    style={{ borderTop: 'solid #d4d4d4 2px', padding: '10px' }}
+                  >
+                    <Col span={5}>Section: {section.section}</Col>
+                    <Col span={5}>
+                      Days:{' '}
+                      {section.days.map((day) => (
+                        <span> {day} </span>
+                      ))}
+                    </Col>
+                    <Col span={5}>Start Time: {section.startTime}</Col>
+                    <Col span={5}>End Time: {section.endTime}</Col>
+                    <Button>Enroll</Button>
+                  </Row>
+                );
+              })}
+            </div>
+          </Col>
+        );
+      })}
+    </Row>
   );
 };
 
---
+++ src/redux/features/student/studentCourseManagement.api.ts
@@ -0,0 +1,37 @@
+import { TQueryParam, TResponseRedux } from '../../../types';
+import { baseApi } from '../../api/baseApi';
+
+const studentCourseApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllOfferedCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: '/offered-courses/my-offered-courses',
+          method: 'GET',
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicSemester: builder.mutation({
+      query: (data) => ({
+        url: '/academic-semesters/create-academic-semester',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllOfferedCoursesQuery } = studentCourseApi;
